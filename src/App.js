806427cc258b17b374370accff4959ff1575cb20
Donice <doniceubaru1@gmail.com>
2022-10-28 12:19:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const buttons = [
+    {
+      linkName: 'Twitter Link',
+      linkAdress: ''
+    },
+    {
+      linkName: 'Zuri Team',
+      linkAdress: ''
+    },
+    {
+      linkName: 'Zuri Books',
+      linkAdress: ''
+    },
+    {
+      linkName: 'Python Books',
+      linkAdress: ''
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
