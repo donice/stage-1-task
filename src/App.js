1b75c703f7fd3065c31b3204e1d679ed4b831576
Donice <doniceubaru1@gmail.com>
2022-10-29 04:30:58 +0100

    added the hover effet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Slack from "./img/slack.svg";
 import Github from "./img/Social_icon.svg";
 import Ingresive from "./img/ingressive.png";
 import Zuri from "./img/zuri.png";
-import { HiOutlineArrowUturnRight } from "react-icons/hi2";
+import Link from "./img/Icon-md.png";
+import { FiCamera } from "react-icons/fi";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import "./index.css";
 
@@ -45,23 +46,35 @@ function App() {
   return (
     <div className="App">
       <div>
-        <div className="hidden md:block right w-10 h-10 mt-4 mr-6 font-bold border-2 border-dashed rounded-full md:mr-20 hover:border-4 hover:border-double">
-          <HiOutlineArrowUturnRight />
+        <div className="rightDesktop hidden md:block w-10 h-10 mt-4 mr-6 font-bold border-2 border-dashed rounded-full md:mr-20 hover:bg-gray-200">
+          <img src={Link} alt="icon" />
         </div>
-        <div className="block md:hidden right w-10 h-10 mt-4 mr-6 font-bold border-2 border-dashed rounded-full md:mr-20 hover:border-4 hover:border-double">
+        <div className="rightMobile block md:hidden w-10 h-10 mt-4 mr-6 font-bold border-2 border-dashed rounded-full md:mr-20">
           <BiDotsHorizontalRounded />
         </div>
       </div>
 
+    <div className="container w-30 flex flex-col sm-auto justify-center items-center">
       <img
         src={ProfileImage}
         alt="Profile"
         id="profile__img"
-        className="mt-8 w-20 rounded-full border-sky-500"
+        className="img mt-8 w-24 rounded-full border-sky-500 md:w-28 md:hover:brightness-50"
       />
+      <div class="overlay">
+        <a href="#" class="icon">
+          <FiCamera />
+        </a>
+      </div>
+    </div>
+      
+
+      <p id="slack" style={{ display: "none" }}>
+        drdonice
+      </p>
       <h1 className="text-2xl py-4 font-bold">Donice Ubaru</h1>
 
-      <div className="w-full">
+      <div className="w-full md:w-3/5">
         {buttons.map((button) => (
           <div className="bg-gray-200 rounded-lg text-sm p-5 my-5 drop-shadow-[0_15px_25px_rgba(0,0,0,0.02)] hover:drop-shadow-md hover:bg-gray-300">
             <a href={button.href} id={button.id}>
@@ -72,7 +85,7 @@ function App() {
       </div>
 
       <div className="flex">
-        <a href="#" type="hidden" id="slack">
+        <a href="https://stage-1-task.vercel.app">
           <img
             src={Slack}
             alt="slack"
